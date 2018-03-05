fa7b0454816023fe6a3c90bdab46fc1ca8c21a9f
Atmadeep Banerjee <atmadeepb@gmail.com>
2018-03-05 16:55:25 +0530

    Add files via upload

---
+++ TweepyMain.py
@@ -0,0 +1,100 @@
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+import argparse
+import string
+import TweepyConfig as config
+import json
+
+
+def get_parser():
+    """Get parser for command line arguments."""
+    parser = argparse.ArgumentParser(description="Twitter Downloader")
+    parser.add_argument("-q", "--query", dest="query", help="Query/Filter", default='-')
+    '''parser.add_argument("-d", "--data-dir", dest="data_dir", help="Output/Data Directory")'''
+    return parser
+
+
+class MyListener(StreamListener):
+    """Custom StreamListener for streaming data."""
+
+    def __init__(self, query):
+        query_fname = format_filename(query)
+        self.outfile = "data/stream_%s.json" % (query_fname)
+    def on_data(self, data):
+        try:
+            
+            with open(self.outfile, 'a') as f:
+                f.write(data)
+            print(data)
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+            time.sleep(5)
+        return True
+
+    def on_error(self, status):
+        print(status)
+        if (status == 420):
+            #returning False in on_data disconnects the stream
+            return False
+        return True
+    def on_status(self, status):
+        if (status.retweeted_status):
+            return
+
+
+def format_filename(fname):
+    """Convert file name into a safe string.
+    Arguments:
+        fname -- the file name to convert
+    Return:
+        String -- converted file name
+    """
+    return ''.join(convert_valid(one_char) for one_char in fname)
+
+
+def convert_valid(one_char):
+    #Set invalid characters to '_' 
+    
+    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+    if one_char in valid_chars:
+        return one_char
+    else:
+        return '_'
+
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+
+
+
+def openStream():	
+    print('opened')
+    #print(config.consumer_key+'\n'+config.consumer_secret+'\n'+config.access_token+'\n'+config.access_secret+'\n')
+    parser = get_parser()
+    args = parser.parse_args()
+    auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_secret)
+    api = tweepy.API(auth)
+    query = [x.strip() for x in args.query.split(',')]#split comma separated queries into list of queries
+	
+    twitter_stream = Stream(auth, MyListener(args.query))
+    twitter_stream.filter(track=query)
+    ''' time.sleep(60)#keep stream open for 60 seconds
+    closeStream(twitter_stream)
+	
+def closeStream(twitter_stream):
+    twitter_stream.disconnect()
+    print('closed')
+    time.sleep(60*5)#give 5 minutes to allow stream to close completely
+    #(call neural net for prediction)
+    time.sleep(60*10)#open stream again after 10 minutes
+    openStream()'''
+
+if __name__ == '__main__':
+  openStream()
---
+++ TweetSentimentAnalysis.py
@@ -0,0 +1,85 @@
+import numpy as np
+import keras.backend as K
+import multiprocessing
+import tensorflow as tf
+import json
+
+from gensim.models.word2vec import Word2Vec
+from gensim.models import KeyedVectors
+
+from keras.callbacks import EarlyStopping
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv1D
+from keras.optimizers import Adam
+from keras.models import load_model
+
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.tokenize import RegexpTokenizer
+import argparse
+def get_parser():
+    """Get parser for command line arguments."""
+    parser = argparse.ArgumentParser(description="Twitter Downloader")
+    '''parser.add_argument("-q", "--query", dest="query", help="Query/Filter", default='-')'''
+    parser.add_argument("-d", "--data-dir", dest="data_dir", help="json Data Directory")
+    return parser
+
+
+if __name__ == '__main__':
+
+    parser = get_parser()
+    args = parser.parse_args()
+
+    data_location = args.data_dir #set this
+
+
+
+    nn_model_location = 'sentimentNet.h5'
+    wv_model_location = 'GoogleNews-vectors-negative300.bin'
+
+
+    with open(data_location,'r', encoding ='utf-8') as t:
+        tweets = []
+        for i,line in enumerate(t):
+            if(i%2==0):
+                tweets.append(json.loads(line)["text"].strip().lower())
+                    
+    # Tokenize and stem
+    tkr = RegexpTokenizer('[a-zA-Z0-9@]+')
+    stemmer = LancasterStemmer()
+
+    tokenized_corpus = []
+
+    for i, tweet in enumerate(tweets):
+        tokens = [stemmer.stem(t) for t in tkr.tokenize(tweet) if not t.startswith('@')]
+        tokenized_corpus.append(tokens)
+            
+    vector_size = 300
+
+    max_tweet_length = 15
+
+
+    word_vecs = KeyedVectors.load_word2vec_format(wv_model_location, binary=True)
+    print('all models loaded')
+    X = np.zeros((len(tokenized_corpus),max_tweet_length, vector_size), dtype=K.floatx())#3D np array
+
+
+    for i in range(len(tokenized_corpus)):
+        for t, token in enumerate(tokenized_corpus[i]):
+            if t >= max_tweet_length:
+                break
+            
+            if token not in word_vecs:
+                continue
+        
+            
+            X[i, t, :] = word_vecs[token]
+    print('word vectors calculated')
+    del word_vecs
+
+    model = load_model(nn_model_location)
+    predictions = model.predict(X)
+    #print(predictions)
+    #predictions = np.nan_to_num(predictions)
+    print("Average sentiment of collected tweets is "+ str(np.average(predictions)))
+    print("Number of tweets processed: "+ str(len(predictions)))
